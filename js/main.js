db4a8d3329cc734baac2608f63baddd7c18a06c0
Your Name <you@example.com>
2019-03-14 12:41:01 +0100

    Index avec Css et Main.js améliorer
    Signed-off-by: Your Name <you@example.com>

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ let tasks = new CompoTaskList({
     element: document.getElementById('tasks'),
     taskList: list
 });
+
+let option1 = new Task("premier de la liste", null);
+let option2 = new Task("Deuxième de la liste", null);
+let option3 = new Task("Troisième de la liste", null);
+
+list.add(option1);
+list.add(option2);
+list.add(option3);
+
+tasks.render();
